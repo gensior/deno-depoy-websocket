6cf9aa0839408b477377015fbb85d684647b3b69
Jesse G. Franceschini <j.franceschini@gmail.com>
2021-12-07 23:50:21 -0800

    Adds user query param checking.

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
-import { serve } from "https://deno.land/std/http/server.ts";
+import { serve } from "https://deno.land/std@0.117.0/http/server.ts";
 
+//@ts-ignore: this is ok
 serve((req) => {
   const upgrade = req.headers.get("upgrade") || "";
   if (upgrade.toLowerCase() != "websocket") {
@@ -7,14 +8,28 @@ serve((req) => {
   }
 
   const { socket, response } = Deno.upgradeWebSocket(req);
-  socket.onopen = () => console.log("socket opened");
+  const url = new URL(req.url);
+  const user = url.searchParams.get("user");
+  if (user == null) {
+    return new Response("request must have a 'user' query parameter.");
+  }
+
+  console.log({ user });
+  socket.onopen = () => processUser(user, socket);
   socket.onmessage = (e) => {
     console.log("socket message:", e.data);
     socket.send(new Date().toString());
   };
+  //@ts-ignore: this is ok
   socket.onerror = (e) => console.log("socket errored:", e.message);
   socket.onclose = () => console.log("socket closed");
   return response;
 });
 
 console.log("Listening on http://localhost:8000");
+
+const processUser = (user: string, socket: WebSocket) => {
+  console.log(user);
+  console.log("socket opened");
+  socket.send(`Hello ${user}!`);
+};
