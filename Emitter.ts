4acc35fd8998786a74d50b0e0b31a508fd8e7beb
Jesse G. Franceschini <j.franceschini@gmail.com>
2022-02-19 23:34:30 -0800

    User mediator.

--- Emitter.ts
+++ Emitter.ts
@@ -1,66 +1,25 @@
 import {
-  LobbyController,
-  LobbyControllerSingleton,
-} from "./Controllers/LobbyController.ts";
-import { Err, Ok, Result } from "./deps.ts";
+  UserController,
+  UserControllerSingleton,
+} from "./Controllers/UserController.ts";
+import { Mediator } from "./deps.ts";
+import { MediatorSingleton } from "./Mediator/Mediators.ts";
 import {
-  IJoinedLobby,
-  ILobbyCreated,
-  InternalEvents,
-  InternalEventsSingleton,
-} from "./Events/InternalEvents.ts";
-import Connection from "./Switchboard/Connection.ts";
+  Broadcast,
+  LobbyCreatedNotification,
+  Send,
+} from "./Mediator/Notifications.ts";
 import {
   Switchboard,
   SwitchboardSingleton,
 } from "./Switchboard/Switchboard.ts";
-import { LobbyWTO } from "./WTOs/LobbyWTO.ts";
 import { PlayerWTO } from "./WTOs/PlayerWTO.ts";
 
 export class Emitter {
-  private lobbyController: LobbyController;
-  private switchboard: Switchboard;
-  private events: InternalEvents;
-
   constructor(
-    switchboard: Switchboard = SwitchboardSingleton,
-    events: InternalEvents = InternalEventsSingleton,
-    lobbyController: LobbyController = LobbyControllerSingleton,
+    private readonly switchboard: Switchboard = SwitchboardSingleton,
+    private readonly mediator: Mediator = MediatorSingleton,
+    private readonly userController: UserController = UserControllerSingleton,
   ) {
-    this.lobbyController = lobbyController;
-    this.switchboard = switchboard;
-    this.events = events;
-
-    this.events.LobbyCreated.attach((x) => {
-      this.lobbyCreatedHandler(x);
-    });
-
-    this.events.JoinedLobby.attach((x) => {
-      this.joinedLobbyHandler(x);
-    });
-  }
-
-  public lobbyCreatedHandler(x: ILobbyCreated): void {
-    const message = JSON.stringify({ id: x.lobbyId });
-    this.switchboard.Send(message, x.creatorId);
-  }
-
-  public joinedLobbyHandler(x: IJoinedLobby): void {
-    // Get lobby
-    this.lobbyController.get(x.lobbyId).andThen((lobby) =>
-      lobby.getPlayer(x.playerId).map((player) => {
-        const user = player.user;
-        this.switchboard.Send(
-          JSON.stringify(LobbyWTO.FromLobby(lobby)),
-          user.id,
-        );
-        lobby.Broadcast(
-          JSON.stringify(PlayerWTO.FromPlayer(player)),
-          player.id,
-        );
-      })
-    ).mapErr((err) => console.error(err));
   }
 }
-
-export const EmitterSingleton = new Emitter();
